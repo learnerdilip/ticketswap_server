52d90f83ef0b7f2a3901b82d0d5e7fc97c3f7427
learnerdilip <diliptalks@gmail.com>
2020-02-25 19:41:27 +0100

    working ticket fetch and display endpoints

--- tickets/router.js
+++ tickets/router.js
@@ -3,21 +3,24 @@ const Ticket = require("./model");
 
 const router = new Router();
 
-router.post("/ticketpost", async (request, response, next) => {
+router.post("/getticketlist", async (request, response, next) => {
   try {
-    const { price, description, title } = request.body.fullTicket;
-    const eventId = request.body.eventId;
-    const createTicket = Ticket.create({ price, description, title, eventId });
+    const eventTickets = await Ticket.findAll({
+      where: {
+        eventId: request.body.eventId
+      }
+    });
+    response.json(eventTickets);
   } catch {
     error => next(console.error);
   }
 });
 
-router.get("/getticketlist", async (request, response, next) => {
+router.post("/ticketpost", async (request, response, next) => {
   try {
-    console.log("the ticket list request--------------------------", request);
-
-    // const
+    const { price, description, title } = request.body.fullTicket;
+    const eventId = request.body.eventId;
+    const createTicket = Ticket.create({ price, description, title, eventId });
   } catch {
     error => next(console.error);
   }
