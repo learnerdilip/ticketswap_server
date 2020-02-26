ec5aeab0df0d60e322aa48b5e65021eb31cdd17c
learnerdilip <diliptalks@gmail.com>
2020-02-27 00:48:57 +0100

    able to find hours of ticket creation

--- tickets/router.js
+++ tickets/router.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const Ticket = require("./model");
 const User = require("../user/model");
 const auth = require("../auth/middleWare");
+// const Sequelize = require("sequelize");
 
 const router = new Router();
 
@@ -20,7 +21,23 @@ router.post("/getticketlist", async (request, response, next) => {
 
 router.post("/ticketpost", auth, async (request, response, next) => {
   try {
-    console.log("----------------------", request.body);
+    //time of creation (given in 24 hr format)
+    const timeHour = new Date(request.body.fullTicket.createdAt);
+    const hora = timeHour.getHours();
+
+    //avg will decide about the 10%
+    const ticketEventArray = await Ticket.findAll({
+      where: { eventId: request.body.eventId }
+    });
+    const sumOfPrices = sum.reduce((sum, item) => {
+      return sum + item.dataValues.price;
+    }, 0);
+    const averageOfEventPrice = Math.floor(ticketEventArray / sumOfPrices);
+    //only one ticket (add 10%)
+    const UserTickets = await User.findAll({
+      where: { userId: request.body.userid }
+    });
+    const userTicketLen = averageOfEventPrice.length;
 
     const { price, description, title } = request.body.fullTicket;
     const eventId = request.body.eventId;
