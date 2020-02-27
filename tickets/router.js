0a1f4202b5eddf5dfc2ec4de3847c3f892e4f38c
learnerdilip <diliptalks@gmail.com>
2020-02-27 11:29:36 +0100

    able to find risk of entered ticket

--- tickets/router.js
+++ tickets/router.js
@@ -21,43 +21,6 @@ router.post("/getticketlist", async (request, response, next) => {
 
 router.post("/ticketpost", auth, async (request, response, next) => {
   try {
-    // //the inital risk can be given based on fixed parameters
-
-    // //time of creation (given in 24 hr format)
-    console.log("****************************************", request.body);
-    // const timeHour = new Date(request.body.fullTicket.createdAt);
-    // const hora = timeHour.getHours();
-    // const lateRiskMeasure = hora > 9 && hora < 17 ? -10 : 10; //(3)
-
-    // //only one ticket (add 10%)
-    // const UserTickets = await User.findAll({
-    //   where: { userId: request.body.userid }
-    // });
-    // const userTicketCount = UserTickets.length;
-    // const isOnlyTicket = userTicketCount <= 1 ? 10 : 0; //(1)
-
-    // //avg will decide about the % RISK above/below
-    // const ticketEventArray = await Ticket.findAll({
-    //   where: { eventId: request.body.eventId }
-    // });
-    // const sumOfPrices = sum.reduce((sum, item) => {
-    //   return sum + item.dataValues.price;
-    // }, 0);
-    // const averageOfEventPrice = Math.floor(ticketEventArray / sumOfPrices);
-    // const percetage = Math.floor(
-    //   (request.body.fullTicket.price * 100) / averageOfEventPrice
-    // );
-    // console.log(
-    //   "the --------params for risk -----",
-    //   lateRiskMeasure,
-    //   isOnlyTicket,
-    //   percetage
-    // );
-
-    // //FINAL RISK(INITIAL)
-    // // const initRiskOfTicket = 90;
-
-    //normal search and response
     const { price, description, title } = request.body.fullTicket;
     const eventId = request.body.eventId;
     const userId = request.body.userid;
@@ -73,6 +36,44 @@ router.post("/ticketpost", auth, async (request, response, next) => {
   } catch {
     error => next(console.error);
   }
+});
+
+router.post("/updaterisk", async (request, response, next) => {
+  // //the inital risk can be given based on fixed parameters
+  // //time of creation (given in 24 hr format)
+  const timeHour = new Date(request.body.createdAt);
+  const hora = timeHour.getHours();
+  const lateRiskMeasure = hora > 9 && hora < 17 ? -10 : 10; //(3)
+
+  //only one ticket (add 10%)
+  const UserTickets = await Ticket.findAll({
+    where: { userId: request.body.userId }
+  });
+  const userTicketCount = UserTickets.length;
+  const isOnlyTicket = userTicketCount <= 1 ? 10 : 0; //(1)
+
+  // //avg will decide about the % RISK above/below
+  const ticketEventArray = await Ticket.findAll({
+    where: { eventId: request.body.eventId }
+  });
+  const sumOfPrices = ticketEventArray.reduce((sum, item) => {
+    return sum + item.dataValues.price;
+  }, 0);
+  const averageOfEventPrice = Math.floor(sumOfPrices / ticketEventArray.length);
+  const percetage = Math.floor(
+    (request.body.price * 100) / averageOfEventPrice
+  );
+  const avgRiskPercentage =
+    averageOfEventPrice > request.body.price ? -percetage : percetage;
+
+  // //FINAL RISK(INITIAL)
+  const initRiskOfTicket = isOnlyTicket + lateRiskMeasure + averageOfEventPrice;
+  // console.log("the RISK IS _______-----------", initRiskOfTicket);
+  //add to the ticket
+  const ticketFind = await Ticket.findByPk(request.body.id);
+  ticketFind.risk = initRiskOfTicket;
+  ticketFind.save();
+  console.log("----------the final ticket with RISK----", ticketFind);
 });
 
 module.exports = router;
