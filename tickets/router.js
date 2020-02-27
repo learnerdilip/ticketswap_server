61e2fe694828bb770463d6fef39755b9b68766da
learnerdilip <diliptalks@gmail.com>
2020-02-27 10:49:32 +0100

    saving before risk algo implementation

--- tickets/router.js
+++ tickets/router.js
@@ -21,24 +21,43 @@ router.post("/getticketlist", async (request, response, next) => {
 
 router.post("/ticketpost", auth, async (request, response, next) => {
   try {
-    //time of creation (given in 24 hr format)
-    const timeHour = new Date(request.body.fullTicket.createdAt);
-    const hora = timeHour.getHours();
+    // //the inital risk can be given based on fixed parameters
 
-    //avg will decide about the 10%
-    const ticketEventArray = await Ticket.findAll({
-      where: { eventId: request.body.eventId }
-    });
-    const sumOfPrices = sum.reduce((sum, item) => {
-      return sum + item.dataValues.price;
-    }, 0);
-    const averageOfEventPrice = Math.floor(ticketEventArray / sumOfPrices);
-    //only one ticket (add 10%)
-    const UserTickets = await User.findAll({
-      where: { userId: request.body.userid }
-    });
-    const userTicketLen = averageOfEventPrice.length;
+    // //time of creation (given in 24 hr format)
+    console.log("****************************************", request.body);
+    // const timeHour = new Date(request.body.fullTicket.createdAt);
+    // const hora = timeHour.getHours();
+    // const lateRiskMeasure = hora > 9 && hora < 17 ? -10 : 10; //(3)
 
+    // //only one ticket (add 10%)
+    // const UserTickets = await User.findAll({
+    //   where: { userId: request.body.userid }
+    // });
+    // const userTicketCount = UserTickets.length;
+    // const isOnlyTicket = userTicketCount <= 1 ? 10 : 0; //(1)
+
+    // //avg will decide about the % RISK above/below
+    // const ticketEventArray = await Ticket.findAll({
+    //   where: { eventId: request.body.eventId }
+    // });
+    // const sumOfPrices = sum.reduce((sum, item) => {
+    //   return sum + item.dataValues.price;
+    // }, 0);
+    // const averageOfEventPrice = Math.floor(ticketEventArray / sumOfPrices);
+    // const percetage = Math.floor(
+    //   (request.body.fullTicket.price * 100) / averageOfEventPrice
+    // );
+    // console.log(
+    //   "the --------params for risk -----",
+    //   lateRiskMeasure,
+    //   isOnlyTicket,
+    //   percetage
+    // );
+
+    // //FINAL RISK(INITIAL)
+    // // const initRiskOfTicket = 90;
+
+    //normal search and response
     const { price, description, title } = request.body.fullTicket;
     const eventId = request.body.eventId;
     const userId = request.body.userid;
